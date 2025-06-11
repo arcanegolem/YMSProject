bb005d1db7d2e3091ace46ec122a894bafa9935b
arcanegolem <dopeflipper@gmail.com>
2025-06-11 04:20:48 +0300

    Анимированный сплэш (без lottie :D)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
   implementation(libs.androidx.navigation.compose)
   implementation(libs.kotlinx.serialization.json)
   implementation(libs.koin.android)
+  implementation(libs.androidx.core.splashscreen)
 
   implementation(project(":data"))
   implementation(project(":domain"))
--- app/src/main/java/arcanegolem/yms/project/MainActivity.kt
+++ app/src/main/java/arcanegolem/yms/project/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import arcanegolem.yms.project.ui.theme.YMSProjectTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    installSplashScreen()
     enableEdgeToEdge(
       statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
       navigationBarStyle = SystemBarStyle.light(Color.WHITE, Color.BLACK)
