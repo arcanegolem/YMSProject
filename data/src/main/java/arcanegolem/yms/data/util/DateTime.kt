c4ff66a45029ff7cc110d98e0958af57fe115baf
arcanegolem <dopeflipper@gmail.com>
2025-07-03 15:23:23 +0300

    Апдейт зависимостей и исправления связанные с ними

--- data/src/main/java/arcanegolem/yms/data/util/DateTime.kt
+++ data/src/main/java/arcanegolem/yms/data/util/DateTime.kt
@@ -1,23 +1,26 @@
 package arcanegolem.yms.data.util
 
-import kotlinx.datetime.Clock
-import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.format
+import kotlinx.datetime.format.Padding
 import kotlinx.datetime.format.char
 import kotlinx.datetime.toInstant
 import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 
 private val YYYYMMDD_LD = LocalDate.Format {
   year()
   char('-')
   monthNumber()
   char('-')
-  dayOfMonth()
+  day(padding = Padding.ZERO)
 }
 
+@OptIn(ExperimentalTime::class)
 internal fun Long.toDateString() : String {
   val instant = Instant.fromEpochMilliseconds(this)
   val localDate = instant.toLocalDateTime(TimeZone.UTC).date
@@ -25,6 +28,7 @@ internal fun Long.toDateString() : String {
   return localDate.format(YYYYMMDD_LD)
 }
 
+@OptIn(ExperimentalTime::class)
 internal fun monthStartMillis() : Long {
   val today = Clock.System.now().toLocalDateTime(TimeZone.UTC)
   val monthStart = LocalDateTime(today.year, today.month, 1, 0, 0, 0)
@@ -32,10 +36,12 @@ internal fun monthStartMillis() : Long {
   return monthStart.toInstant(TimeZone.UTC).toEpochMilliseconds()
 }
 
+@OptIn(ExperimentalTime::class)
 internal fun todayMillis() : Long {
   return Clock.System.now().toEpochMilliseconds()
 }
 
+@OptIn(ExperimentalTime::class)
 internal fun String.parseMillis() : Long {
   return Instant.parse(this).toEpochMilliseconds()
 }
